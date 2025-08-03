8a42a27ff9a048dda9e7db30a18a1324e174e9bd
SwiftLATAM <comunidadswiftlatam@gmail.com>
2025-08-03 17:05:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Futuras funciones JS aquí
+// Auto scroll del carrusel
+const carousel = document.querySelector('.carousel');
+
+let scrollSpeed = 1;
+let interval = null;
+
+function startScrolling() {
+  interval = setInterval(() => {
+    carousel.scrollLeft += scrollSpeed;
+    if (carousel.scrollLeft >= carousel.scrollWidth - carousel.clientWidth) {
+      carousel.scrollLeft = 0;
+    }
+  }, 20);
+}
+
+function stopScrolling() {
+  clearInterval(interval);
+}
+
+carousel.addEventListener('mouseenter', stopScrolling);
+carousel.addEventListener('mouseleave', startScrolling);
+
+startScrolling();
